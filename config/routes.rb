f8b2dd2aacda6845450f966c650f61fa0d33f5f2
kztklztk <bjkk1290470@gn.iwasaki.ac.jp>
2021-07-03 19:01:40 +0900

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/" => "post#home"
+  get "/home" => "post#home"
+  get "/top_artists" => "post#top_artists"
+  get "/top_tracks" => "post#top_tracks"
+  get "/top_genres" => "post#top_genres"
 end
